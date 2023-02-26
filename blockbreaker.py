61e9e09e5e8d771546f3895ef79a1334d3ad8c9f
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2023-02-26 06:46:19 -0800

    Changed blocks to use list instead of array

--- blockbreaker.py
+++ blockbreaker.py
@@ -5,9 +5,15 @@ from block import block
 class blockbreaker:
     def __init__(self) -> None:
         self.player = player()
-        self.rows = 4
-        self.cols = 8
-        self.blocks = [[block(i, j) for j in range(self.rows)] for i in range(self.cols)]
+        self.rows = 1
+        self.cols = 1
+        self.blocks = list()
+        self.initBlocks()
+
+    def initBlocks(self):
+        for i in range(self.cols):
+            for j in range(self.rows):
+                self.blocks.append(block(i, j))
 
     def update(self):
         #player
@@ -17,23 +23,11 @@ class blockbreaker:
     def display(self, screen, pos):
         screen.fill("black")
         self.player.display(screen, pos)
-        for i in range(self.cols):
-            for j in range(self.rows):
-                self.blocks[i][j].display(screen)
+        for block in self.blocks:
+            block.display(screen)
         
     def blockCollision(self):
         if not self.player.hasBall:
-            for i in range(self.cols):
-                for j in range(self.rows):
-                    block = self.blocks[i][j]
-                    if block.x <= self.player.ball.x and block.x + block.width >= self.player.ball.x:
-                        if self.player.ball.y >= block.y:
-                            self.player.ball.speedx = self.player.ball.speedx * -1
-                        if self.player.ball.y <= block.y + block.height:
-                            self.player.ball.speedx = self.player.ball.speedx * -1
-                    if block.y <= self.player.ball.y and block.y + block.height >= self.player.ball.y:
-                        if self.player.ball.x >= block.x:
-                            self.player.ball.speedy = self.player.ball.speedy * -1
-                        if self.player.ball.x <= block.x + block.width:
-                            self.player.ball.speedy = self.player.ball.speedy * -1
-                    
+            for block in self.blocks:
+                if block.x < self.player.ball.x and block.x + block.width > self.player.ball.x:
+                    # if block.y
